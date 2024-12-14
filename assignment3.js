9ea9a5637acc7cbb019d0640d990293a763a179d
shatha003 <shathakhlifat@gmail.com>
2024-12-14 13:55:37 +0300

    Update assignment3.js

--- assignment3.js
+++ assignment3.js
@@ -1,21 +1,29 @@
-const url="https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100"
+const url =
+  "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
 
-async function fetchUOBData(){
-    try{
-        const response= await fetch(url)
-        if(!response.ok || response.status !=200){
+// function to fetch data from the API
+async function fetchUOBData() {
+  try {
+    // get the data from the API
+    const response = await fetch(url);
 
-            console.error('not ok')
-          
-        }
-
-       console.log('response=', response);
-       
-         console.log(data);
-       
-
+    // check if the response is successful and stop if the response is successful
+    if (!response.ok || response.status != 200) {
+      console.error("Error: Response not okay");
+      return; 
     }
-    catch(error){
-        console.error('error occured', error); 
-    }
+
+    // convert the response to JSON format
+    const data = await response.json();
+    console.log("Response data:", data); 
+
+    // extract the records from the data
+    const results = data.records;
+
+    // pass the records to the function that displays them
+    displayUobData(results);
+  } catch (error) {
+    // show an error message if something goes wrong
+    console.error("Error occurred:", error);
+  }
 }
